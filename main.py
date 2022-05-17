9efcc2045793636ff3333256eaf3ca0c5ebbfc3c
moieis <87827220+moieis@users.noreply.github.com>
2022-05-17 04:38:30 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+import time
+
+from pywebio.input import *
+from pywebio.output import *
+from pywebio import start_server
+import pandas as pd
+
+def start():
+    s=input('dsfhsd',type=TEXT)
+    put_text(s)
+    popup('',s)
+    data = pd.DataFrame({'hi':[23]})
+    time.sleep(9)
+    put_html(data)
